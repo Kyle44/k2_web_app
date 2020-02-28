b97e538b9bd700e8b47f1ff08ece19a7025f1ab1
Kyle <kyle.primary@gmail.com>
2020-02-28 17:31:05 -0500

    Updates

---
+++ precache-manifest.5f4c8a2fc1ae33d7ddc22ac1fd82af95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb2a1d68fae60125f49d8e38e63656f",
+    "url": "/k2_web_app/index.html"
+  },
+  {
+    "revision": "c2f2809d07476ff3b321",
+    "url": "/k2_web_app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "33ab96cce7a7c5cf48f5",
+    "url": "/k2_web_app/static/js/2.5bdcc25a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/k2_web_app/static/js/2.5bdcc25a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f2809d07476ff3b321",
+    "url": "/k2_web_app/static/js/main.0fcf4b19.chunk.js"
+  },
+  {
+    "revision": "dc1464b886feefea98fc",
+    "url": "/k2_web_app/static/js/runtime-main.c6ac475b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/k2_web_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
